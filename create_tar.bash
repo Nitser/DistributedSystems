ba02c445b5b489903310fddf20a4b5a41199facd
bellavin.anton <bellavin.anton@itrium.ru>
2019-04-27 15:16:59 +0300

    change tar bash

--- create_tar.bash
+++ create_tar.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
-mkdir pa2
-cp *.h *c pa2
-tar -czvf pa2.tar.gz pa2/
-rm -Rf pa2/
+if [ -z $1 ]; then
+    echo "write lab number"
+    exit 1
+fi
+
+mkdir "pa$1"
+cp *.h *c "pa$1"
+tar -czvf "pa$1.tar.gz" "pa$1"/
+rm -Rf "pa$1"/
