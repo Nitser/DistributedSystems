bf091831301f23c3889d8a163f437076425a241f
bellavin.anton <bellavin.anton@itrium.ru>
2019-06-07 00:21:23 +0300

    fix tar script

--- unpacking_tar.bash
+++ unpacking_tar.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ -d "pa2_results/" ]; then
-	rm -Rf "pa2_results"
+if [ -d "pa$1_results/" ]; then
+	rm -Rf "pa$1_results"
 fi
-echo "12345" | gpg --passphrase-fd 0 -d pa2_results.tar.gz.gpg > pa2_results.tar.gz
-tar -xvf pa2_results.tar.gz
-rm pa2_results.tar.gz.gpg pa2_results.tar.gz
+echo "12345" | gpg --passphrase-fd 0 -d "pa$1_results.tar.gz.gpg" > "pa$1_results.tar.gz"
+tar -xvf "pa$1_results.tar.gz"
+rm "pa$1_results.tar.gz.gpg" "pa$1_results.tar.gz"
 
